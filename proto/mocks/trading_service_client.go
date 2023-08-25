af51245beffa6874ed21f3ff5044aff907762756
artnikel <artyom.leskco@gmail.com>
2023-08-25 17:08:18 +0300

    mock proto

--- proto/mocks/trading_service_client.go
+++ proto/mocks/trading_service_client.go
@@ -47,6 +47,36 @@ func (_m *TradingServiceClient) ClosePosition(ctx context.Context, in *proto.Clo
 	return r0, r1
 }
 
+// GetPrices provides a mock function with given fields: ctx, in, opts
+func (_m *TradingServiceClient) GetPrices(ctx context.Context, in *proto.GetPricesRequest, opts ...grpc.CallOption) (*proto.GetPricesResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *proto.GetPricesResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *proto.GetPricesRequest, ...grpc.CallOption) *proto.GetPricesResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*proto.GetPricesResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *proto.GetPricesRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetProfit provides a mock function with given fields: ctx, in, opts
 func (_m *TradingServiceClient) GetProfit(ctx context.Context, in *proto.GetProfitRequest, opts ...grpc.CallOption) (*proto.GetProfitResponse, error) {
 	_va := make([]interface{}, len(opts))
